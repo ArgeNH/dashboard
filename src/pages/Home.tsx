7caf7dee462f530a6db11483ed4b40164d107b32
ArgeNH <arge.nino@uptc.edu.co>
2024-03-22 15:00:06 -0500

    feat: add location

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,14 @@
-import { Outlet } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 export const Home = () => {
+  const location = useLocation();
   return (
-    <div>
-      <h1>Home</h1>
-      <Outlet />
+    <div className="flex justify-center items-center h-full">
+      <h1 className="text-3xl">
+        <code className="bg-secondary-200 p-2 rounded-md">
+          {location.pathname}
+        </code>
+      </h1>
     </div>
   );
 };
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,14 @@
+import { useLocation } from 'react-router-dom';
+
 export const NotFound = () => {
-  return <div>NotFound</div>;
+  const location = useLocation();
+  return (
+    <div className="flex justify-center items-center h-full">
+      <h1 className="text-3xl">
+        <code className="bg-secondary-200 p-2 rounded-md">
+          {location.pathname}
+        </code>
+      </h1>
+    </div>
+  );
 };
